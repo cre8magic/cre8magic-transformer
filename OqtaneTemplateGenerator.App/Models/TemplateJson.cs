427830141df07b46d1ae0c6be93d5f88328c7f1f
Tonći Vatavuk <tonci@sistemi.hr>
2025-07-02 17:15:50 +0200

    Update settings and template properties

--- OqtaneTemplateGenerator.App/Models/TemplateJson.cs
+++ OqtaneTemplateGenerator.App/Models/TemplateJson.cs
@@ -3,8 +3,8 @@ namespace ToSic.Cre8magic.Oqtane.TemplateGenerator.Models;
 
 public class TemplateJson
 {
-    public string Title { get; set; } = "[Theme]";
+    public string Title { get; set; } = "Theme Template";
     public string Type { get; set; } = "External";
-    public string Version { get; set; } = "[FrameworkVersion]";
+    public string Version { get; set; } = "6.1.3";
     public string Namespace { get; set; } = "[Owner].Theme.[Theme]";
 }
